72d4b96cf404607090e1d76417d07982e0a0d05c
Matthew <Matirix@live.com>
2024-04-30 12:06:37 -0700

    added customButton.jsx and main screen

--- app/index.jsx
+++ app/index.jsx
@@ -1,14 +1,52 @@
 import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
-import {Link} from 'expo-router';
+import { ScrollView, Text, View, Image } from 'react-native';
+import {Redirect, router} from 'expo-router';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import {images} from '../constants'
+import CustomButton from '../components/customButton';
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-        <Text className="text-3xl">Aora</Text>
-        <Link href="home" className='bg-blue-100'>Home</Link>
-        
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+        <ScrollView contentContainerStyle={{height:'100%'}}>
+            <View className="h-min-[85vh] w-full items-center px-4 justify-center">
+                {/* Logo */}
+                <Image
+                    source={images.logo}
+                    className="w-[130px] h-[84px]"
+                    resizeMode='contain'/>
+                {/* Cards */}
+                <Image 
+                    source={images.cards}
+                    className="max-w-[380px] w-full h-[300px]"
+                    resizeMode='contain'/>
+
+                {/* Text Boxes */}
+                <View className="relative mt-5">
+                    {/* Text */}
+                    <Text className="text-3xl text-white font-bold items-center">
+                        Discover Endless Possibilities with {' '}
+                        <Text className="text-secondary-200">Aora</Text>
+                    </Text>
+                    {/* Underlined */}
+                    <Image source={images.path} className="w-[136px] h-[15px] absolute -bottom-2 -right-8"
+                    resizeMode="contain"/>
+
+                    <Text className="text-gray-100 text-sm font-pregular mt-7 text-center">
+                        Where creativity meets inovation: embark on a journey of endless possibilities with Aora.
+                    </Text>
+                </View>
+
+                {/* Button */}
+                <CustomButton 
+                    title="Continue with email"
+                    handlePress={()=>router.push('/sign-in')}
+                    containerStyles="w-full mt-7"/>
+            </View>
+            
+
+        </ScrollView>
+        <StatusBar backgroundColor="#161622"style="light"/>
+    </SafeAreaView>
   );
 }
